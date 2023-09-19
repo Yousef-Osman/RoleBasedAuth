61ee8e54980ee14c53c8c15d20f9dd358e3a56c6
Yousef Osman <yousef.osman.31@gmail.com>
2023-09-19 15:49:06 +0300

    add role manager index

---
+++ RoleBasedAuth/Controllers/AdminController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RoleBasedAuth.Data;
+
+namespace RoleBasedAuth.Controllers;
+public class AdminController : Controller
+{
+    private readonly ApplicationDbContext _context;
+    private readonly RoleManager<IdentityRole> _roleManager;
+
+    public AdminController(ApplicationDbContext context, RoleManager<IdentityRole> roleManager)
+    {
+        _context = context;
+        _roleManager = roleManager;
+    }
+
+    public IActionResult Index()
+    {
+        var model = _roleManager.Roles.ToList();
+        return View(model);
+    }
+
+    public IActionResult Roles()
+    {
+        var model = _roleManager.Roles.ToList();
+        return View(model);
+    }
+
+    public IActionResult AddRole()
+    {
+        var model = new IdentityRole();
+        return View(model);
+    }
+
+    [HttpPost]
+    public IActionResult AddRole(IdentityRole model)
+    {
+
+        return View(model);
+    }
+}
+
